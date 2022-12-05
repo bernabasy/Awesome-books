5c3b448e7f808971092d46fdb3d75a4d90141c25
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-05 15:23:09 +0300

    Add UI  class

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const Form = document.querySelector('#book-form');
-const title = document.querySelector('#title').value;
-const author = document.querySelector('#author').value;
+const UI = {
+    displayBooks(){
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book) );
+    },
+   addBookToList(book){
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td><a hraf="#" class="btn btn-sm btn-danger delete">remove</a></td>
+    `;
+    list.appendChild(row);
+ },
+
+  deleteBook(el){
+ if(el.classList.contains('delete')) {
+    el.parentElement.parentElement.remove();
+ }
+ },
+
+    clearfields(){
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+ },
+};
+
+// Store
