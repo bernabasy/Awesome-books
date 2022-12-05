c26c5c4f8c6264797efa06faa58efb28aed8378d
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-05 20:18:46 +0300

    Fixe eslint error in app.js file

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-const UI = {
-    displayBooks(){
+const Store = {
+  getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  },
+
+  addBook(book) {
     const books = Store.getBooks();
-    books.forEach((book) => UI.addBookToList(book) );
-    },
-   addBookToList(book){
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  },
+
+  removeBook(author) {
+    const books = Store.getBooks();
+    let index = 0;
+    books.filter((book) => {
+      if (book.author !== author) 
+        {index =+ ; }
+      return books;
+    });
+    books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+  },
+};
+const UI = {
+  displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+  },
+  addBookToList(book) {
     const list = document.querySelector('#book-list');
 
     const row = document.createElement('tr');
@@ -14,73 +43,41 @@ const UI = {
     <td><a href="#" class="btn btn-sm btn-danger delete">remove</a></td>
     `;
     list.appendChild(row);
- },
+  },
 
-  deleteBook(el){
- if(el.classList.contains('delete')) {
-    el.parentElement.parentElement.remove();
- }
- },
+  deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  },
 
-    clearField(){
+  clearField() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
- },
-};
-
-const Store = {
-    getBooks() {
-       let books;
-       if(localStorage.getItem('books') === null){
-           books =[];
-       } else{
-        books = JSON.parse(localStorage.getItem('books'));
-      }
-      return books;
-   },
-    
-    addBook(book) {
-const books = Store.getBooks();
- books.push(book);
- localStorage.setItem('books', JSON.stringify(books));
-   },
-
-   removeBook(author){
-  const books = Store.getBooks();
-  let index = 0;
-  books.filter(book => {
-   if(book.author != author)
-    index ++;
-    else
-    return;
-  } )
-  books.splice(index, 1);
-  localStorage.setItem('books', JSON.stringify(books));
-   },
+  },
 };
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 const Form = document.querySelector('#book-form');
-Form.addEventListener('submit', (e)=> {
-e.preventDefault();
+Form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-const title = document.querySelector('#title').value;
-const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
- const book = {
-    author : author,
-    title : title,
-   }
+  const book = {
+    author,
+    title,
+  };
 
-   UI.addBookToList(book);
+  UI.addBookToList(book);
 
-   Store.addBook(book);
+  Store.addBook(book);
 
-   UI.clearField();
+  UI.clearField();
 });
 
-document.querySelector('#book-list').addEventListener('click', (e) =>{
-    Store.removeBook(e.target.parentElement.previousElementSibling.innerHTML);
-     UI.deleteBook(e.target);  
- });
- 
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  Store.removeBook(e.target.parentElement.previousElementSibling.innerHTML);
+  UI.deleteBook(e.target);
+});
